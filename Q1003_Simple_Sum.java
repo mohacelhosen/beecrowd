1407ccd1bad0ea036a5a690ee7c2af58b730f667
mohacelhosen <mohacel.hosen22@gmail.com>
2023-03-04 11:26:47 +0600

    Q1003 Simple Sum

---
+++ Q1003_Simple_Sum.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Q1003_Simple_Sum {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+        int SOMA = A+B;
+        System.out.printf("SOMA = %d\n",SOMA);
+    }
+}
