c5f3185c33036cf7a7f1dff8438cf6d540c01934
mohacelhosen <mohacel.hosen22@gmail.com>
2023-03-04 11:06:10 +0600

    Q1001 Extremly Basic

---
+++ Q10001_Extremely_Basic.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Q10001_Extremely_Basic {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+        int X = A + B;
+        System.out.printf("X = %d\n",X);
+    }
+}
