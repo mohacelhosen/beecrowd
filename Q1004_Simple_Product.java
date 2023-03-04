f9cb1a654b496192a4b450c5f9d864cd367065dd
mohacelhosen <mohacel.hosen22@gmail.com>
2023-03-04 11:32:46 +0600

    Q1004 Simple Product

---
+++ Q1004_Simple_Product.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Q1004_Simple_Product {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int B = in.nextInt();
+        int PROD = A*B;
+        System.out.printf("PROD = %d\n",PROD );
+    }
+}
