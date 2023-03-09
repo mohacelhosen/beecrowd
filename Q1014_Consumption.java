2f430b8994dce97addc4a973632524debb80c655
mohacelhosen <mohacel.hosen22@gmail.com>
2023-03-09 17:09:21 +0600

    Q1014 Consumption

---
+++ Q1014_Consumption.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Q1014_Consumption {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int distance = in.nextInt();
+        double fuel  = in.nextDouble();
+        double consumption = distance/fuel;
+        System.out.printf("%f.3 km/l\n",consumption);
+
+       
+    }
+}
