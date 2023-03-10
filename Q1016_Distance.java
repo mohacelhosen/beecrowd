c5a4a6b5511e75e3bf32d96293534450d01a0573
mohacelhosen <mohacel.hosen22@gmail.com>
2023-03-10 09:31:12 +0600

    Q1016 Distance

---
+++ Q1016_Distance.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Q1016_Distance {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int distance = 2*n;
+        System.out.printf("%d minutos\n",distance);
+ 
+    }
+ 
+}
