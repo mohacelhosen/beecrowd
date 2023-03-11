43f2f077a0e7d2f6d1b37b47fb7eb623b41999e1
mohacelhosen <mohacel.hosen22@gmail.com>
2023-03-11 09:45:30 +0600

    Q1038 Even Numbers

---
+++ Q1059_Even_Numbers.java
@@ -0,0 +1,10 @@
+
+public class Q1059_Even_Numbers {
+    public static void main(String[] args) {
+        for(int i=1; i<=100; i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
