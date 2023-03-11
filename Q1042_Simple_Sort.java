8278bbcbdab0d5780f14800d575b3a089fac605e
mohacelhosen <mohacel.hosen22@gmail.com>
2023-03-12 02:36:05 +0600

    Simple Sort

--- Q1042_Simple_Sort.java
+++ Q1042_Simple_Sort.java
@@ -1,18 +1,35 @@
-import java.util.Arrays;
+
 import java.util.Scanner;
 public class Q1042_Simple_Sort {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int[] arr = new int[3];
-        int[] temp = new int[3];
-        for(int i=0; i<arr.length; i++){
-            arr[i]=in.nextInt();
-            temp[i]=arr[i];
-        }
-        
-        for(int i=0; i<arr.length; i++){
 
-        }
+        int a= in.nextInt();
+        int b= in.nextInt();
+        int c= in.nextInt();
+
+        arr[0]=a;
+        arr[1]=b;
+        arr[2]=c;
+        
+       for(int i=0; i<2; i++){
+            for(int j=i+1; j<3;j++){
+                if(arr[i]>arr[j]){
+                    int temp = arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+       }
+       for(int i:arr){
+        System.out.println(i);
+       }
+       System.out.println();
+       System.out.println(a);
+       System.out.println(b);
+       System.out.println(c);
+
 
 
     }
