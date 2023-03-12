aac6b9ab5aa4a5a1d65d26ddb19218f57e01f271
mohacelhosen <mohacel.hosen22@gmail.com>
2023-03-12 09:14:27 +0600

    Q1044 Multiples

---
+++ Q1044_Multiples.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Q1044_Multiples {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        if(b/a==0){
+            System.out.println("Sao Multiplos");
+        }else{
+            System.out.println("Nao sao Multiplos");
+        }
+    }
+}
