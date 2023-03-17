37d629348ef3137f1821fe694bfe140b74c72c7d
mohacelhosen <mohacel.hosen22@gmail.com>
2023-03-17 20:19:19 +0600

    Q1095_Sequence_IJ_1.java

---
+++ Q1095_Sequence_IJ_1.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+public class Q1095_Sequence_IJ_1 {
+ public static void main(String[] args) throws IOException {
+   
+    for(int j=60, i=1; j>=0; j-=5, i+=3){
+        System.out.printf("I=%d J=%d\n",i,j);
+    }
+ }   
+}
