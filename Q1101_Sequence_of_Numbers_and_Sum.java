ff1a6060a34746f05708e1f8452450ad160f7444
mohacelhosen <mohacel.hosen22@gmail.com>
2023-03-19 16:46:16 +0600

    Q1101 Sequence of Numbers and sum

--- Q1101_Sequence_of_Numbers_and_Sum.java
+++ Q1101_Sequence_of_Numbers_and_Sum.java
@@ -3,15 +3,19 @@ import java.util.Scanner;
 public class Q1101_Sequence_of_Numbers_and_Sum {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int m = in.nextInt();
-        int n = in.nextInt();
+        int m=in.nextInt();
+        int n=in.nextInt();
         int sum =0;
-        if(n>m){int temp=n; n=m; m=n; }
-        while(m<=0 || n<=0){
+        while( m>0 && n>0){
+            if(n>m){ int temp = n; n=m; m=temp;}
             for(int i=n; i<=m; i++){
-                sum=sum+i;
+                sum+=i;
+                System.out.print(i+" ");
             }
+            System.out.println("Sum="+sum);
+            sum=0;
+            m=in.nextInt();
+            n=in.nextInt();
         }
-        System.out.println(sum);
     }
 }
