cde01035f5fc469fc64708319b797bbb086dd9e1
mohacelhosen <mohacel.hosen22@gmail.com>
2023-04-06 05:37:29 +0600

    1134 - Type of Fuel

--- Q1134_Type_of_Fuel.java
+++ Q1134_Type_of_Fuel.java
@@ -3,26 +3,33 @@ import java.util.Scanner;
 public class Q1134_Type_of_Fuel {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int type = in.nextInt();
+
         int alcool = 0;
         int gasoline = 0;
         int diesel = 0;
-        System.out.println("MUITO OBRIGADO");
-        while(type!=4){
-            if(type==1){ 
-                alcool++; 
-                type=in.nextInt();}
-            else if(type==2){
-                gasoline++;  
-                type=in.nextInt();}
-            else if(type==3){
-                diesel++;
-                type=in.nextInt();
-                }
-            else{type=in.nextInt();}
+        while(true){
+            int num = in.nextInt();
+            if (num == 4){
+                break;
+            }
+            else if (num == 1){
+                alcool += 1;
+            }
+            else if (num == 2){
+                gasoline += 1;
+            }
+            else if (num == 3){
+                diesel += 1;
+            }
+            else{
+                continue;
+            }
         }
-        System.out.printf("Alcool: %d\n",alcool);
-        System.out.printf("Gasoline : %d\n",gasoline); 
-        System.out.printf("Diesel : %d\n", diesel);
+        System.out.println("MUITO OBRIGADO");
+        System.out.printf("Alcool: %d\n", alcool);
+        System.out.printf("Gasolina: %d\n", gasoline);
+        System.out.printf("Diesel: %d\n", diesel);
+
+        in.close();
     }
 }
